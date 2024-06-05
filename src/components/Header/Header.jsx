e2ab341621251b1d28776aec638766c55fac79c6
RaymondAfan <raymond.afan@icloud.com>
2024-06-04 18:46:49 -0700

    branch

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,7 @@ const Header = ({}) => {
   return (
     <header className="header">
       <div className="header__logo">
-        <div>HEADER</div>
+        <div>HEADER sick</div>
       </div>
     </header>
   );

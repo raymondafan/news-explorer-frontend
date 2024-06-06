7358d434525346b91104987e6f42d13f026edc05
RaymondAfan <raymond.afan@icloud.com>
2024-06-05 23:40:57 -0700

    header styling and downloading fonts

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,17 @@
+import logoImage from "../../assets/whitelogo.svg";
+import "./Header.css";
 const Header = ({}) => {
   return (
     <header className="header">
       <div className="header__logo">
-        <div>HEADER sick</div>
+        <img src={logoImage} alt="logo" />
+      </div>
+      <div className="header__user-container">
+        <button className="header__button-home">Home</button>
+
+        <button className="header__button-signin" type="text">
+          Sign in
+        </button>
       </div>
     </header>
   );

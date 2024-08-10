054612bc0ddc1b9cf8bba2a496bcfac8a3ba28f2
raymondafan <raymond.afan@icloud.com>
2024-08-09 18:16:41 -0700

    token

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -2,23 +2,29 @@ const signIn = (email, password) => {
   // Pretend we did a fetch request that gave us back a token
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      resolve({ token: "a fake token" });
+      const token = "simulated-token";
+      resolve({ token });
     }, 1000);
   });
 };
 const signUp = (email, password, username) => {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      resolve({ token: "a fake token" });
+      const token = "simulated-token";
+      resolve({ token });
     }, 1000);
   });
 };
 const checkToken = (token) => {
   // Pretend we did a fetch request that gave us back a user
   return new Promise((resolve, reject) => {
-    resolve({
-      data: { name: "fake user", email: "fake@example.com", id: "fake-id" },
-    });
+    if (token === "simulated-token") {
+      resolve({
+        data: { name: "fake user", email: "fake@example.com", id: "fake-id" },
+      });
+    } else {
+      reject("Invalid Token");
+    }
   });
 };
 
